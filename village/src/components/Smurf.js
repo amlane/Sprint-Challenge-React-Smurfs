60fcd7ff588f47a31a63fb5787f198243a27153f
amlane <twelvexstring@gmail.com>
2019-05-10 12:52:35 -0500

    refactored useless class component

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-class Smurf extends React.Component{
-  constructor(props){
-    super(props);
-  }
-
-  render(){
+function Smurf(props){
   return (
     <div className="Smurf">
-     <Link to={`/smurf/${this.props.id}`}><h3>{this.props.name}</h3></Link> 
-      <strong>{this.props.height} tall</strong>
-      <p>{this.props.age} smurf years old</p>
-      <button onClick={e => this.props.deleteSmurf(e, this.props.id)}>X</button>
+     <Link to={`/smurf/${props.id}`}><h3>{props.name}</h3></Link> 
+      <strong>{props.height} tall</strong>
+      <p>{props.age} smurf years old</p>
+      <button onClick={e => props.deleteSmurf(e, props.id)}>X</button>
     </div>
   );
-  }
 };
 
 
